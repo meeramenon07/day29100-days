dcfe3ed9dc3fa51aad478d2047de6bba09e38de2
meeramenon07 <46912602+meeramenon07@users.noreply.github.com>
2023-11-28 12:10:10 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+def newPrint(color, word):
+  if color=="red":
+    print("\033[31m", word, sep="", end="")
+  elif color=="green":
+    print("\033[32m", word, sep="", end="")
+  elif color=="blue":
+    print("\033[34m", word, sep="", end="")
+  elif color =="purple":
+    print("\033[35m", word, sep="", end="")
+  
+    
+  else:
+    print("\033[0m", word, sep="", end="")
+
+print("Super Subroutine")
+print("With my ", end="")
+newPrint("red", "new program")
+newPrint("reset", " I can just call red and ")
+newPrint("red", "it will print in red ")
+newPrint("blue", "or even blue ")
+newPrint("green","it will print in green ")
+newPrint("purple", " it will print in purple ")
+
+
+
+
+
+
+
+
+
